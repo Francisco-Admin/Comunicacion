7cac45a0798ac8152a5edba7ee08a39b424ba1ff
Francisco-Admin <64043910+Francisco-Admin@users.noreply.github.com>
2020-04-27 03:07:40 -0500

    Initial commit

---
+++ app/src/main/java/com/edu/isc/tesoem/francisco/comunicacionactividades/frmUno.java
@@ -0,0 +1,17 @@
+package com.edu.isc.tesoem.francisco.comunicacionactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frmUno extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_uno);
+    }
+
+}
